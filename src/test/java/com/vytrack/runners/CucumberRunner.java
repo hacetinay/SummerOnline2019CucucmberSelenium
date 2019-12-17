5e96706426600dbf0ae4271129377a29ea32f18b
hacetinay <hacetinay@gmail.com>
2019-12-17 11:38:42 -0500

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,13 @@
+package com.vytrack.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "C:/Users/hacet/IdeaProjects/SummerOnline2019CucucmberSelenium/src/test/resources/features"
+)
+public class CucumberRunner {
+}
